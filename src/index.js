034f4dc0ca056d41b4fe621b87d410ea924c4437
Lee Samuel <Lsamuel94947@gmail.com>
2026-04-09 02:46:12 -0700

    Finalize forum functionality and resolve environment display warnings

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; 
-import './App.css';
+import App from './App.jsx';
 
 const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (!rootElement) {
+    console.error("Failed to find the root element.");
+} else {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+}
