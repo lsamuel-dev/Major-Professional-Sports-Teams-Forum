b1c22eb28f6a0c182a1c89aa21f59948954cfd4c
Lee Samuel <Lsamuel94947@gmail.com>
2026-04-11 00:06:23 -0700

    vite

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/Major-Professional-Sports-Teams-Forum/',
 })
