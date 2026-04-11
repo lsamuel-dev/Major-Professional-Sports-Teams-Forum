0dc1c96cf277d1d32220883d652b4cb6ba5f0166
Lee Samuel <Lsamuel94947@gmail.com>
2026-04-11 00:53:34 -0700

    index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 
 const rootElement = document.getElementById('root');
 
 if (!rootElement) {
-    console.error("Failed to find the root element.");
+    console.error('Failed to find the root element.');
 } else {
     const root = ReactDOM.createRoot(rootElement);
     root.render(
       <React.StrictMode>
-        <App />
+        <BrowserRouter basename='/Major-Professional-Sports-Teams-Forum'>
+          <App />
+        </BrowserRouter>
       </React.StrictMode>
     );
 }
